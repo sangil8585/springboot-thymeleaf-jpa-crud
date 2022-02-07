8e34d958fa7cd9b692ba262f19a89b326f8eacc8
sangil8585 <sangil8585@naver.com>
2022-02-07 17:28:00 +0900

    Initial Commit

---
+++ src/main/java/boot/jpa/crud/SpringbootThymeleafJpaCrudApplication.java
@@ -0,0 +1,13 @@
+package boot.jpa.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafJpaCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThymeleafJpaCrudApplication.class, args);
+    }
+
+}
